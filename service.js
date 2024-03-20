11fbc955d1e7a49b6741138cf5aa60d8a77abd7a
AnalystVishal <164136165+AnalystVishal@users.noreply.github.com>
2024-03-20 11:25:19 +0530

    Create service.js
    App Script file for configuring GCS

---
+++ service.js
@@ -0,0 +1,14 @@
+// Replace these with your own values
+const service_account = {
+  private_key: '-----BEGIN PRIVATE KEY-----\<your key>',
+  client_email: '<your GCS service account email>',
+};
+
+const getStorageService = () =>
+  OAuth2.createService('FirestoreStorage')
+    .setPrivateKey(service_account.private_key)
+    .setIssuer(service_account.client_email)
+    .setPropertyStore(PropertiesService.getUserProperties())
+    .setCache(CacheService.getUserCache())
+    .setTokenUrl('https://oauth2.googleapis.com/token')
+    .setScope('https://www.googleapis.com/auth/devstorage.read_write');
